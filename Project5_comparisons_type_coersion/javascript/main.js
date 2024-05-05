2c202933e09c977b284f27342e7a769f549a3af4
Rocvio01 <162543781+Rocvio01@users.noreply.github.com>
2024-05-04 21:59:18 -0700

    Add files via upload

---
+++ Project5_comparisons_type_coersion/javascript/main.js
@@ -0,0 +1,25 @@
+document.write("15" +7);
+document.write(10==1=);
+document.write(3==11);
+
+x = 82; 
+Y = "82";
+document.write(X===Y);
+
+document.write(7 > 5 && 10 > 3);
+document.write(5 > 10 && 10 > 3);
+document.write(9 > 7 || 10 > 4);
+document.write(5 > 10 || 10 > 20);
+
+console.log(1===1);
+console.log(2=== "1")
+
+
+
+
+function not_function() {
+    document.getElementById("Not") .innerHTML = ! (20 > 10);
+    function not_function() {
+        document.getElementById("Not") .innerHTML = !(5 > 10);
+    }
+}
